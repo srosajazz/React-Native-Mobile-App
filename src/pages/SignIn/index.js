52ebeb211dc4aa0c0bf582c3ac5614eccee7c396
Sergio Rosa <scarlosjazz@gmail.com>
2019-09-08 08:54:21 -0400

    implenting Input & Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  return (
+    <Background>
+      <Text>SignIn</Text>
+
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="Enter your name"
+      />
+
+      <Button>Enter</Button>
+    </Background>
+  );
 }
